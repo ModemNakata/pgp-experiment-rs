04073a248eec21efd29c0555e7987b21593b862f
Nakata Modem <nakatamodem@hotmail.com>
2026-05-18 02:13:37 +0200

    encrypt/decrypt version commented out

--- src/main.rs
+++ src/main.rs
@@ -1,29 +1,42 @@
 use pgp::{
-    composed::{ArmorOptions, EncryptionCaps, KeyType, SecretKeyParamsBuilder, SignedSecretKey},
+    composed::{
+        ArmorOptions, EncryptionCaps, KeyType, SecretKeyParamsBuilder, SignedSecretKey,
+        SubkeyParamsBuilder,
+    },
     crypto::ecc_curve::ECCCurve,
 };
 use rand::thread_rng;
 
 fn main() {
-    // Generate a single, ultra-slim primary key with ZERO subkeys
-    let secret_key = keygen_slim("user@example.com").expect("failed during slim keygen");
+    let secret_key = keygen_with_encryption("user@example.com").expect("failed during keygen");
 
     let armor_opts = ArmorOptions::default();
     println!("{}", secret_key.to_armored_string(armor_opts).unwrap());
 }
 
-fn keygen_slim(uid: &str) -> Result<SignedSecretKey, pgp::composed::SubkeyParamsBuilderError> {
-    let mut key_params = SecretKeyParamsBuilder::default();
+fn keygen_with_encryption(
+    uid: &str,
+) -> Result<SignedSecretKey, pgp::composed::SubkeyParamsBuilderError> {
+    // 1. Build the ultra-slim Encryption Subkey using Cv25519 (ECDH)
+    // let mut encrypt_subkey = SubkeyParamsBuilder::default();
+    // encrypt_subkey
+    //     .key_type(KeyType::ECDH(ECCCurve::Curve25519)) // Curve25519 for encryption
+    //     .can_sign(false)
+    //     .can_encrypt(EncryptionCaps::All) // Enables full encryption/decryption
+    //     .can_authenticate(false);
 
-    // Consolidate all capabilities into the primary master key
+    // 2. Build the primary master key (handles Sign, Certify, Auth)
+    let mut key_params = SecretKeyParamsBuilder::default();
     key_params
-        .key_type(KeyType::Ed25519Legacy) // Compact, modern Edwards curve
-        .can_certify(true) // Must have to be a valid master key
-        .can_sign(true) // Handles data signing (e.g., Git commits)
-        .can_authenticate(true) // Handles SSH authentication
-        .can_encrypt(EncryptionCaps::None) // Ed25519 cannot natively encrypt data
-        .primary_user_id(uid.into())
-        .subkeys(vec![]); // <--- Completely empty! No subkeys.
+        .key_type(KeyType::Ed25519Legacy)
+        .can_certify(true)
+        .can_sign(true)
+        .can_authenticate(true)
+        .can_encrypt(EncryptionCaps::None)
+        .primary_user_id(uid.into());
+    // .subkeys(vec![
+    // encrypt_subkey.build()?, // Attach the single encryption subkey
+    // ]);
 
     let secret_key_params = key_params.build().expect("Build secret_key_params");
     let signed = secret_key_params
